781fa8bc9cf414f022acfc18e837bb18e54a9f09
John G <gentijo@gmail.com>
2022-02-16 18:53:11 -0800

    update

--- components/8266_IOWidget/DeviceConfig.h
+++ components/8266_IOWidget/DeviceConfig.h
@@ -3,20 +3,16 @@
 #define DEVCONFIG_h
 
 
+#define SDA_PIN 4
+#define SCL_PIN 5
+
 #define I2C_MASTER_ADDR 0x42
 #define I2C_SLAVE_ADDR  0x08
 
-
 #define BTS7960_enabled true
-
-  #define SDA_PIN 4
-  #define SCL_PIN 5
-
 
 #if  BTS7960_enabled == true
   #include "BTS7960.h"
-  #define SDA_PIN 4
-  #define SCL_PIN 5
 #endif
 
 #endif
