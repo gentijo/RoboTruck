524ea19ded33948d630b9d25a33cd60a4cb4e54f
John Gentilin <gentijo@pop-os.localdomain>
2022-03-16 08:56:15 -0700

    working on the linux version

--- docker/ROS/startNodeUnix.sh
+++ docker/ROS/startNodeUnix.sh
@@ -1,11 +1,14 @@
 NODENAME=$1
 WORKSPACE=$2
 
-docker run -it --privileged \
+docker run --privileged -it \
 --name $NODENAME  \
 -h $NODENAME \
---env DISPLAY=unix$DISPLAY \
--v $XAUTH:/root/.Xauthority \
+--net=host \
+--env DISPLAY=$DISPLAY \
+--env XAUTHORITY=/root/.Xauthority \
+-v $XAUTHORITY:/root/.Xauthority \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
--v /home/gentijo/Projects/ROS-Projects/tutorial_ws/src/ros_tutorials:/opt/ros/dev-ws/src/ros_tutorials \
-galactic-ros-base-xwin:latest
+rosimg bash
+
+#-v /home/gentijo/Projects/ROS-Projects/tutorial_ws/src/ros_tutorials:/opt/ros/dev-ws/src/ros_tutorials \
